21497b25b9431ea2ab4f95d501e841b263aa6df4
damian4166 <damian4166@tlen.pl>
2026-05-31 07:41:15 +0200

    pwa

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+        registerType: 'autoUpdate', // Aplikacja sama się zaktualizuje u użytkownika
+        manifest: {
+          name: 'Szkolny Shoutbox PRO',
+          short_name: 'Shoutbox',
+          description: 'Klasowy komunikator czasu rzeczywistego',
+          theme_color: '#8e44ad',
+          background_color: '#f4f7f6',
+          display: 'standalone',
+          icons: [
+            {
+              src: 'icon-192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'icon-512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ],
 })
